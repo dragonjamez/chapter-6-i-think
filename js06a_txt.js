285d53929226474ca6ec3b1003d1884f5ba7108d
jamey <jamey_brandon@icloud.com>
2026-03-02 14:05:56 -0500

    thing

--- js06a_txt.js
+++ js06a_txt.js
@@ -10,22 +10,22 @@
       Filename: js06a.js
  */
 window.addEventListener("load", function(){
-      calcOrder()
       let orderForm = this.document.forms.orderForm
       let model = orderForm.elements.model
       model.focus()
       for (let i = 0; i < orderForm.elements.length; i++) {
             orderForm.elements[i].addEventListener("change", calcOrder)
+      calcOrder()
       }
 })
 function calcOrder(){
       let mIndex = model.selectedIndex
-      let mValue = model.options [mIndex].mValue
+      let mValue = model.options[mIndex].value
       let qIndex = orderForm.elements.qty.selectedIndex
-      let quantity = orderForm.elements.qty[qIndex].mValue
+      let quantity = orderForm.elements.qty[qIndex].value
       let modelCost = mValue*quantity
       orderForm.elements.modelCost.value = modelCost.toLocaleString("en-US", {style:"currency",currency: "USD"})
-      let planValue = document.querySelector(('input[name="plan"]:checked').value)
+      let planValue = document.querySelector('input[name="plan"]:checked').value
       let planCost = planValue * quantity
       orderForm.elements.planCost.value = planCost.toLocaleString("en-US", {style:"currency",currency: "USD"})
       let subtotal = modelCost + planCost
@@ -36,7 +36,7 @@ function calcOrder(){
       orderForm.elements.totalCost.value = totalCost.toLocaleString("en-US", {style:"currency",currency: "USD"})
       orderForm.elements.modelName.value = model.options[mIndex].text
       let selectedPlan = document.querySelector('input[name="plan"]:checked')
-      orderForm.elements.PlanName.value = selectedPlan.labels[0].textContent
+      orderForm.elements.planName.value = selectedPlan.labels[0].textContent
 }
 
 
